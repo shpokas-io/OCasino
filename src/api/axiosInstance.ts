685e9f0366fa972d969f82331cafa503952abf65
shpokas <s.shpokas@gmail.com>
2025-02-12 16:32:08 +0200

    chore: typo fix, import axios

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,10 @@
+import axios from "axios";
+
 const api = axios.create({
   baseURL: "http://localhost:3000",
 });
 
-api.interceptos.request.use((config) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
