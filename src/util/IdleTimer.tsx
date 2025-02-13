677f01fa513471c3daf8977cd0b341482cd0510e
shpokas <s.shpokas@gmail.com>
2025-02-13 19:05:19 +0200

    feat: add timer logic for inactivity

---
+++ src/util/IdleTimer.tsx
@@ -0,0 +1,46 @@
+// THis logic should be in backend, but not to start messing with mockup-api i will create logout timer here
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../store/authSlice";
+import { RootState } from "../store/store";
+import { useNavigate } from "react-router-dom";
+
+const IdleTimer: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { accessToken } = useSelector((state: RootState) => state.auth);
+  const inactivityLimit = 10 * 60 * 1000;
+  const lastActivityRef = useRef(Date.now());
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    const resetTimer = () => {
+      lastActivityRef.current = Date.now();
+    };
+
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("keydown", resetTimer);
+    window.addEventListener("click", resetTimer);
+    window.addEventListener("scroll", resetTimer);
+
+    const interval = setInterval(() => {
+      if (Date.now() - lastActivityRef.current > inactivityLimit) {
+        dispatch(logout());
+        navigate("/login");
+      }
+    }, 1000);
+
+    return () => {
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("keydown", resetTimer);
+      window.removeEventListener("click", resetTimer);
+      window.removeEventListener("scroll", resetTimer);
+      clearInterval(interval);
+    };
+  }, [accessToken, dispatch, navigate]);
+
+  return null;
+};
+
+export default IdleTimer;
